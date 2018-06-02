d9559fcad24869d20db8e7987b81c512e1c47040
Joel Lee <lee.joel395@gmail.com>
2018-06-02 04:05:54 -0700

    Initial commit + Line base class

---
+++ Splines/Assets/Editor/LineInspector.cs
@@ -0,0 +1,43 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace JLProject.Spline{
+    /// <summary>
+    /// used to display a line in the editor
+    /// </summary>
+    [CustomEditor(typeof(Line), true)]
+    public class LineInspector : Editor{
+        private void OnSceneGUI(){
+            Line line = target as Line;
+
+            //alows the transform of the base object to move the line as well
+            Transform handleTransform = line.transform;
+            //allows the rotation to move the line and not the points of the line
+            Quaternion handleRotation = handleTransform.rotation;
+
+            //convert the local points into world space
+            Vector3 p0 = handleTransform.TransformPoint(line.p0);
+            Vector3 p1 = handleTransform.TransformPoint(line.p1);
+
+            //draw a line in worldspace
+            Handles.color = Color.white;
+            Handles.DrawLine(line.p0, line.p1);    
+            
+            //apply any changes made to the handles back to the line itself
+            EditorGUI.BeginChangeCheck();
+            p0 = Handles.DoPositionHandle(p0, handleRotation);
+            if (EditorGUI.EndChangeCheck()){
+                Undo.RecordObject(line, "Move Point"); //allows the object to be undone
+                EditorUtility.SetDirty(line); //marks the object as changed when altered
+                line.p0 = handleTransform.InverseTransformPoint(p0); //converts from worldspace to local space to handle the transform changes
+            }
+            EditorGUI.BeginChangeCheck();
+            p1 = Handles.DoPositionHandle(p1, handleRotation);
+            if (EditorGUI.EndChangeCheck()) {
+                Undo.RecordObject(line, "Move Point");
+                EditorUtility.SetDirty(line);
+                line.p1 = handleTransform.InverseTransformPoint(p1);
+            }
+        }
+    }
+}
---
+++ Splines/Assets/Script/Line.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace JLProject.Spline{
+    /// <summary>
+    /// a base class to describe a line.
+    /// </summary>
+    public class Line : MonoBehaviour{
+        public Vector3 p0, p1;
+    }
+}
