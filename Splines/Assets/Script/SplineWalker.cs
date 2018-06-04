bfa92755a681ad0109588cf89be15089e69f09d4
Joel Lee <lee.joel395@gmail.com>
2018-06-03 17:24:22 -0700

    Completed Project

---
+++ Splines/Assets/Script/SplineWalker.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using JLProject.Spline;
+using UnityEngine;
+
+public class SplineWalker : MonoBehaviour{
+    public enum WalkerMode{
+        Once, 
+        Loop,
+        PingPong
+    }
+
+    public WalkerMode mode;
+    private bool goingForward = true;
+
+    public bool lookForward;
+    public BezierSpline spline;
+    public float duration;
+    private float progress;
+
+    private void Update(){
+        if (goingForward){
+            progress += Time.deltaTime / duration;
+            if (progress > 1f){
+                if (mode == WalkerMode.Once){
+                    progress = 1f;
+                }
+                else if (mode == WalkerMode.Loop){
+                    progress -= 1f;
+                }
+                else{
+                    progress = 2f - progress;
+                    goingForward = false;
+                }
+            }
+        }
+        else{
+            progress -= Time.deltaTime / duration;
+            if (progress < 0){
+                progress = -progress;
+                goingForward = true;
+            }
+        }
+
+        Vector3 position = spline.GetPoint(progress);
+        transform.localPosition = position;
+        if (lookForward) {
+            transform.LookAt(position + spline.GetDirection(progress));
+        }
+    }
+}
